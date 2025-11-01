c6d0efb1d063dbd11c5f8eea3dca253c6c929154
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-11-01 17:52:40 -0300

    cria componente de cartas do baralho

---
+++ src/componentes/Carta.tsx
@@ -0,0 +1,31 @@
+// TODO: ainda falta colocar os valores que cada carta representa no 21
+// os números é o valor deles mesmo
+// rei, rainha, valete tem valor 10
+// As pode valer 1 ou 11
+
+interface CartaPadrao {
+    naipe: 'Paus' | 'Espada' | 'Ouros' | 'Copas'
+    valor: string; // As, rei, rainha, valete e números
+    face_para_cima?: boolean;
+}
+
+function Carta({naipe, valor, face_para_cima = true}: CartaPadrao) {
+    if (!face_para_cima)
+        return <div className="carta para-baixo">verso da carta</div>;
+
+    return (
+        // <div className={`carta carta-${naipe}`}>
+        <div className={"carta carta-" + naipe}>
+
+            <div className="valor-carta">{valor}</div>
+            <div className="naipe-carta">
+                {naipe === 'Paus' && '♣'}
+                {naipe === 'Espada' && '♠'}
+                {naipe === 'Ouros' && '♦'}
+                {naipe === 'Copas' && '♥'}
+            </div>
+        </div>
+    )
+}
+
+export default Carta;
