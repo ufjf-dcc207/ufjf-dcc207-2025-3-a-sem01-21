83c382afa48d0c878baa9b2bf09ec1dc921f827f
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-11-01 17:20:38 -0300

    cria componente da mesa do jogo e adiciona ela ao app

---
+++ src/componentes/MesaBlackjack.tsx
@@ -0,0 +1,14 @@
+
+function MesaBlackjack() {
+    return (
+        <div className="mesa-blackjack">
+            <h2>Mesa de Blackjack(21)</h2>
+            <h3>cartas</h3> 
+            <button>comprar</button>
+            <button>parar</button>
+            <button>passar</button>
+        </div>
+    )
+}
+
+export default MesaBlackjack
