04954b721ab5fcab956775d7abca4565fe32c6c5
joao-nilson <joao.nilson@estudante.ufjf.br>
2025-11-01 19:56:44 -0300

    constroi mesa simples com dados estaticos

--- src/componentes/MesaBlackjack.tsx
+++ src/componentes/MesaBlackjack.tsx
@@ -1,13 +1,27 @@
 import "./MesaBlackjack.css"
+import Mao from "./Mao";
 
 function MesaBlackjack() {
+
+    const cartasDealer = [
+        {naipe: 'Copas', valor: 'A'},
+        {naipe: 'Espadas', valor: 'K'}
+    ];
+    
+    const cartasJogador = [
+        {naipe: 'Ouros', valor: '10'},
+        {naipe: 'Paus', valor: '7'}
+    ]
     return (
         <div className="mesa-blackjack">
             <h2>Mesa de Blackjack(21)</h2>
+            <Mao cartas={cartasDealer} titulo="Dealer"></Mao>/
+            <Mao cartas={cartasJogador} titulo="Jogador"></Mao>
+
             <h3>cartas</h3> 
-            <button>comprar</button>
+            <button>pedir carta</button>
             <button>parar</button>
-            <button>passar</button>
+            <button>dobrar aposta</button>
         </div>
     )
 }
