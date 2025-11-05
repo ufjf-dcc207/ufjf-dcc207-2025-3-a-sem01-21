fab62dde07e89e10fd351578b83f2f3c920a73ae
Gabhriel <gabhriel.velasco@estudante.ufjf.br>
2025-11-04 21:52:21 -0300

    Coloco a chamada de Saldo em App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import MesaBlackjack from './componentes/MesaBlackjack'
+import Saldo from './componentes/Saldo'
 
 function App() {
   return (
     <div className='App'>
       <h1>♠️ Bem-vindo ao jogo de 21 ♣️</h1>
       <MesaBlackjack />
+      <Saldo valor={0}/>
     </div>
   )
 }
